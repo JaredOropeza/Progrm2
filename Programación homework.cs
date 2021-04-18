3ac6eb4777ccd54720115a95c6cccedae65c9edb
JaredOropeza <82776914+JaredOropeza@users.noreply.github.com>
2021-04-18 15:36:36 -0400

    Add files via upload

---
+++ Programación homework.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Programacion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola progrm2!");
+        }
+    }
+}
